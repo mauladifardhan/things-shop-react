5162c89d05b9dfb063cc91d12f52f8160e8a780a
fardhan.mauladi <fardhan.mauladi@yokke.co.id>
2022-11-15 14:43:49 +0700

    first settings

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import css from "./Header.module.css";
+import logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
+
+const Header = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>
+        <img src={logo} alt="" />
+        <span>ThingShop</span>
+      </div>
+      <div className={css.right}>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+        <input type="" className={css.search} />
+        <CgShoppingBag className={css.cart} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
